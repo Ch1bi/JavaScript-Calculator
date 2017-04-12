c97289957d239b0dbfe56d380c8c5e920cbfa8ce
ch1bi <maine1191@gmail.com>
2017-04-11 20:48:26 -0400

    added functions

---
+++ calc.js
@@ -0,0 +1,19 @@
+function add(){
+
+    //  console.log("lets add");
+}
+
+function subtract(){
+
+    //  console.log("lets subtract");
+}
+
+function divide(){
+
+    //  console.log("lets divide");
+}
+
+function multiply(){
+
+    //  console.log("lets multiply");
+}
